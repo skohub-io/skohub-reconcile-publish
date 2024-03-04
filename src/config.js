bd79080e924c9f039fcb835f03a1e49324595de8
@s.roertgen <sroertgen@gmail.com>
2024-03-04 09:33:40 +0100

    Add login with orcid and wikimedia to provide account #19

--- src/config.js
+++ src/config.js
@@ -11,6 +11,16 @@ export const config = {
   es_pass: process.env.ES_PASSWORD,
   es_index: process.env.ES_INDEX,
   reconcile_service_url: process.env.RECONCILE_SERVICE_URL,
+  wikimedia_auth_url: process.env.WIKIMEDIA_AUTH_URL,
+  wikimedia_token_url: process.env.WIKIMEDIA_TOKEN_URL,
+  wikimedia_id: process.env.WIKIMEDIA_ID,
+  wikimedia_secret: process.env.WIKIMEDIA_SECRET,
+  orcid_auth_url: process.env.ORCID_AUTH_URL,
+  orcid_token_url: process.env.ORCID_TOKEN_URL,
+  orcid_getuser_url: process.env.ORCID_GETUSER_URL,
+  orcid_id: process.env.ORCID_ID,
+  orcid_secret: process.env.ORCID_SECRET,
+  contact_mail: process.env.CONTACT_MAIL,
 }
 
 if (result.error) {
